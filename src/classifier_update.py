c5fd4b96d8014400cd4518ec28f3fa6ebe7944b5
An Minh Hùng <35797529+anminhhung@users.noreply.github.com>
2019-11-18 12:25:06 +0700

    Add files via upload

---
+++ src/classifier_update.py
@@ -0,0 +1,75 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+import argparse
+import facenet
+import os
+import sys
+import math
+import pickle
+from sklearn.svm import SVC
+#import tachTestTrain as t
+
+batch_size = 1000
+image_size = 160
+
+def main():
+    with tf.Graph().as_default():
+        with tf.Session() as sess:
+            np.random.seed(seed=666)
+            dataset = facenet.get_dataset("DataSet/FaceData/Processed")
+                 
+            paths, labels = facenet.get_image_paths_and_labels(dataset)
+
+            # Load the model
+            print('Loading feature extraction model')
+            facenet.load_model("DataSet/FaceData/Model/20180402-114759.pb")
+	        
+            # Get input and output tensors
+            images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+            embedding_size = embeddings.get_shape()[1]
+            
+            # Run forward pass to calculate embeddings
+            print('Calculating features for images')
+            nrof_images = len(paths)
+            nrof_batches_per_epoch = int(math.ceil(1.0*nrof_images / batch_size))
+            emb_array = np.zeros((nrof_images, embedding_size))
+            for i in range(nrof_batches_per_epoch):
+                start_index = i*batch_size
+                end_index = min((i+1)*batch_size, nrof_images)
+                paths_batch = paths[start_index:end_index]
+                images = facenet.load_data(paths_batch, False, False, image_size)
+                feed_dict = { images_placeholder:images, phase_train_placeholder:False }
+                emb_array[start_index:end_index,:] = sess.run(embeddings, feed_dict=feed_dict)
+            
+            classifier_filename_exp = os.path.expanduser("DataSet/FaceData/Model/facemodels.pkl")
+  
+            #Train classifier
+            print('Training classifier')
+            model = SVC(kernel='linear', probability=True)
+            model.fit(emb_array, labels)
+                
+            # Create a list of class names
+            class_names = [ cls.name.replace('_', ' ') for cls in dataset]
+
+            with open(classifier_filename_exp, 'wb') as outfile:
+                pickle.dump((model, class_names), outfile)
+            print('Saved classifier model to file "%s"' % classifier_filename_exp)             
+            
+def split_dataset(dataset, min_nrof_images_per_class, nrof_train_images_per_class):
+    train_set = []
+    test_set = []
+    for cls in dataset:
+        paths = cls.image_paths
+        # Remove classes with less than min_nrof_images_per_class
+        if len(paths)>=min_nrof_images_per_class:
+            np.random.shuffle(paths)
+            train_set.append(facenet.ImageClass(cls.name, paths[:nrof_train_images_per_class]))
+            test_set.append(facenet.ImageClass(cls.name, paths[nrof_train_images_per_class:]))
+    return train_set, test_set
+#main()
